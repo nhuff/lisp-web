f931b53068a1f415f5cbe7968b4eced09108efa1
Nathan Huff <nrhuff@umn.edu>
2021-12-18 07:36:51 -0700

    Build executable

--- guix.scm
+++ guix.scm
@@ -1,24 +1,38 @@
 (use-modules (guix build-system asdf)
              (gnu packages)
              (gnu packages lisp-xyz)
+             (gnu packages lisp)
              (guix packages)
              (guix licenses)
              (ice-9 rdelim)
-             (ice-9 popen))
+             (ice-9 popen)
+             (guix gexp))
+
 
 (define-public lisp-web
   (package
    (name "lisp-web")
    (version (string-take (read-string (open-pipe "git show HEAD | head -1 | cut -d ' ' -f 2" OPEN_READ)) 7))
    (source (local-file (dirname (current-filename)) #:recursive? #t))
-   (license bsd)
+   (license bsd-2)
    (home-page "https://www.example.com")
    (synopsis "web demo")
    (description "common lisp web demo")
-   (build-system asdf-build-system)
    (inputs `(("sbcl" ,sbcl)
-             ("bordeaux-threads" ,sbcl-bourdeax-threads)
-             ("hunchentoot" ,sbcl-hunchentoot)))))
+             ("bordeaux-threads" ,sbcl-bordeaux-threads)
+             ("hunchentoot" ,sbcl-hunchentoot)))
+   (build-system asdf-build-system/sbcl)
+   (arguments '(#:asd-systems '("webdemo")
+                #:phases
+                (modify-phases %standard-phases
+                               (add-after 'create-asdf-configuration 'build-program
+                                          (lambda* (#:key outputs #:allow-other-keys)
+                                            (build-program
+                                             (string-append (assoc-ref outputs "out") "/bin/webdemo")
+                                             outputs
+                                             #:entry-program '((app:main))))))))))
+                                                   
+                
 
 lisp-web
 
